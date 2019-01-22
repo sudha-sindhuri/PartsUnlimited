70c252996510c76df24568631d312c998dc25f6a
Colin Dembovsky <colindembovsky@gmail.com>
2019-01-21 16:30:54 -0800

    Adding deploy agents script

---
+++ k8s/env/deploy-azdo-agents.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# parameters
+account=$1
+token=$2
+pool=$3
+replicas=$4
+aksName=$5
+rgName=$6
+
+# get the k8s cluster creds and write them to a local kubeconfig file
+az aks get-credentials -n $aksName -g $rgName
+
+git clone https://github.com/Azure/helm-vsts-agent.git
+helm init --kube-context $aksName
+AZDO_TOKEN=$(echo -n $token | base64)
+helm install --kube-context $aksName \
+  --name azdo-agents \
+  ./helm-vsts-agent \
+  --set vstsToken=$AZDO_TOKEN \
+  --set vstsAccount=$account \
+  --set vstsPool=$pool \
+  --set replicas=$replicas \
+  --set resources.limits.cpu=0 \
+  --set resources.requests.cpu=0
