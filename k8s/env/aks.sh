7544dbb1329fe35d5e46c96f00e2722c5c5e17fb
Colin Dembovsky <colindembovsky@gmail.com>
2019-01-23 15:08:16 -0800

    Fixing echos

--- k8s/env/aks.sh
+++ k8s/env/aks.sh
@@ -10,9 +10,9 @@ kubeVersion="1.11.5"
 # assumes you're logged in already - if running in AzDO, use Azure CLI task with endpoint
 
 # create resource group
-$echo "Ensure resource group exists"
+echo "Ensure resource group exists"
 az group create -n $rgName -l $location --tags owner=colind app=k8spu
 
 # create the ACR enabling admin access
-$echo "Create AKS"
+echo "Create AKS"
 az aks create -n $aksName -g $rgName -l $location -k $kubeVersion --node-count $nodeCount
