2ae7ffbd1a2b2227454d582ad2b035c22ecba555
Colin Dembovsky <colindembovsky@gmail.com>
2019-01-24 17:22:22 -0800

    Add cluster exists check

--- k8s/env/aks.sh
+++ k8s/env/aks.sh
@@ -16,5 +16,10 @@ echo "Ensure resource group exists"
 az group create -n $rgName -l $location --tags owner=colind app=k8spu
 
 # create the ACR enabling admin access
-echo "Create AKS"
-az aks create -n $aksName -g $rgName -l $location -k $kubeVersion --node-count $nodeCount --generate-ssh-keys --service-principal $spnAppId --client-secret $spnSecret
+aksExists=$(az aks list --query "[].name" | grep $aksName)
+if [ "$aksExists" = "" ]; then
+    echo "Create AKS"
+    az aks create -n $aksName -g $rgName -l $location -k $kubeVersion --node-count $nodeCount --generate-ssh-keys --service-principal $spnAppId --client-secret $spnSecret
+else
+    echo "Cluster $aksName exists"
+fi    
