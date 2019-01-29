5384209b5c73fa1578d8e26b4a25a481d08e1042
Colin Dembovsky <colindembovsky@gmail.com>
2019-01-28 21:36:14 -0800

    Update script

--- k8s/env/get-canary-version.sh
+++ k8s/env/get-canary-version.sh
@@ -8,13 +8,26 @@ helmReleaseName=$4
 appName=$5
 canary=$6
 varName=$7
+buildNum=$8
 
 # get the k8s cluster creds and write them to a local kubeconfig file
 echo "Getting credentials for cluster $askName"
 az aks get-credentials -n $aksName -g $rgName
 
-echo "Getting release version for $helmReleaseName-$appName-$canary in $namespace"
-version=$(kubectl get deploy $helmReleaseName-$appName-$canary -n $namespace -o jsonpath="{.spec.template.spec.containers[?(@.name=='$appName')].image}" | sed 's/.*://')
 
-echo "Setting variable $varName with value $version"
+az aks get-credentials -n $(AKSName) -g $(RGName)
+installed=$(helm ls | grep $(Namespace)-api | wc -l)
+if [ "$installed" = "1" ]; then
+  echo "Getting release version for $helmReleaseName-$appName-$canary in $namespace"
+  version=$(kubectl get deploy $helmReleaseName-$appName-$canary -n $namespace -o jsonpath="{.spec.template.spec.containers[?(@.name=='$appName')].image}" | sed 's/.*://')
+else
+  echo "No install - use current build number"
+  version="$buildNum"
+fi
+if [ "$version" = "" ]; then
+  echo "Unable to determine version - default to build num $buildNum"
+  version="$buildNum"
+fi
+
+echo "$canary version is $version"
 echo "##vso[task.setvariable variable=$varName]$version"
