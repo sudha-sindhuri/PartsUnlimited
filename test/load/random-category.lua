3246cedcf2bd2c852fa44da9ec56cce7fcfd33df
Colin Dembovsky <colindembovsky@gmail.com>
2019-02-21 13:00:56 -0800

    Fixing load test script

--- test/load/random-category.lua
+++ test/load/random-category.lua
@@ -7,10 +7,17 @@ products = {
 }
 weightedCategory = 3
 weightedPercentage = 75
+threads = {}
+
+function setup(thread)
+    table.insert(threads, thread)
+end
 
 function getRandomProduct()
     local catId = weightedCategory
-    if weightedCategory == 0 or math.random() * 100 < weightedPercentage then catId = math.random(#products) end
+    local randomNum = math.random() * 100
+    if weightedCategory == 0 or randomNum > weightedPercentage then catId = math.random(#products) end
+    catCounter[catId] = catCounter[catId] + 1
     local category = products[catId]
     return category[math.random(#category)]
 end
@@ -19,7 +26,8 @@ end
 -- e.g. wrk -t1 -c1 -d10 -R10 -s random-category.lua http://localhost:1337 -- arg1 arg2
 -- 1st arg is weighted category (1-indexed) [or 0 for none]
 -- 2nd arg is weighted percentage
-function init(args) 
+function init(args)
+    catCounter = { 0, 0, 0, 0, 0 }
     if #args == 2 then
         weightedCategory = tonumber(args[1])
         weightedPercentage = tonumber(args[2])
@@ -35,6 +43,25 @@ end
 -- add a random product to the end of the request
 request = function()
     local path = wrk.path .. getRandomProduct(weightedCategory, weightedPercentage)
-    --print(path)
+    print(path)
     return wrk.format(wrk.method, path, wrk.headers, wrk.body)
+end
+
+done = function()
+    -- display the distribution of random requests
+    local totalCounter = { 0, 0, 0, 0, 0 }
+    local total = 0
+    for i, thread in ipairs(threads) do
+        local tCatCounter = thread:get("catCounter")
+        for i, c in ipairs(tCatCounter) do
+            totalCounter[i] = totalCounter[i] + c
+            total = total + c
+        end
+    end
+
+    for i, c in ipairs(totalCounter) do
+        percentage = c / total * 100
+        print (string.format("Category %d percentage %.2f", i, percentage))
+    end
+    print "done!"
 end
