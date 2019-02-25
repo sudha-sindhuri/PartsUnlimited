3eacc42dd8e88e10b65c42711d4f31a7793b70d6
Colin Dembovsky <colindembovsky@gmail.com>
2019-02-25 14:03:40 -0800

    Adding replace logic

--- k8s/env/create-sql-connectionstring-secret.sh
+++ k8s/env/create-sql-connectionstring-secret.sh
@@ -11,11 +11,21 @@ passwordSecretName=$7
 
 # get the k8s cluster creds and write them to a local kubeconfig file
 echo "Getting credentials for cluster $askName"
-az aks get-credentials -n $aksName -g $rgName
+#az aks get-credentials -n $aksName -g $rgName
 
 # get the password value
-$password=$(kubectl get -n $namespace secret $passwordSecretName -o jsonpath="{.data.sapassword}" | base64 --decode)
+echo "Get the password for the SQL service"
+password=$(kubectl get -n $namespace secret $passwordSecretName -o jsonpath="{.data.sapassword}" | base64 --decode)
 
 # create the secret
-$constr="Data Source=$serverName;Initial Catalog=$dbName;User Id=sa;Password=$password"
-kubectl create secret generic $secretName --from-literal=connectionstring=$constr -n $namespace
+echo "Creating the connection string secret"
+constr="Data Source=$serverName;Initial Catalog=$dbName;User Id=sa;Password=$password"
+
+exists=$(kubectl get secret -n $namespace | grep $secretName -w)
+if [ "$exists" = "" ]; then
+    echo "Creating connection string secret $secretName"
+    kubectl create secret generic $secretName --from-literal=connectionstring="$constr" -n $namespace
+else
+    echo "Replacing connection string secret $secretName"
+    kubectl create secret generic $secretName --from-literal=connectionstring="$constr" -n $namespace --dry-run -o yaml | kubectl replace -f -
+fi
